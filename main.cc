16dfc7e7bd6fdf920289a689cfb9855337582a91
Eduardo Argandoña <eduardo.argandonasantander@hitachivantara.com>
2024-10-04 15:23:44 +0100

    init

---
+++ main.cc
@@ -0,0 +1,24 @@
+#include <seastar/core/app-template.hh>
+#include <seastar/core/coroutine.hh>
+#include <seastar/util/log.hh>
+
+namespace {
+seastar::logger lg("hello-world");
+}
+
+static seastar::future<> hello_from_all_cores() {
+    for (unsigned i = 0; i < seastar::smp::count; ++i) {
+    co_await seastar::smp::submit_to(i, [] {
+        lg.info("App is running");
+        });
+    }
+    co_return;
+}
+
+int main(int argc, char** argv) {
+    seastar::app_template app;
+    return app.run(argc, argv, [&]() -> seastar::future<int> {
+        co_await hello_from_all_cores();
+        co_return 0;
+    });
+}
